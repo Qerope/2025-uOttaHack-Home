873906acd73837519ec34c1441f27207cd09ad6b
mbelk059 <mbelk818@gmail.com>
2024-08-03 12:32:52 -0400

    add glowy stars

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,4 +1,3 @@
-// components/HeroSection.tsx
 import React from 'react';
 import styles from '../styles/HeroSection.module.css';
 import gridImage from '../assets/Grid.png';
@@ -9,6 +8,11 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
   return (
     <section className={styles.container}>
       <img src={gridImage.src} alt="Background Grid" className={styles.backgroundImage} />
+      <div className={styles.stars}>
+        {Array.from({ length: 23 }).map((_, i) => (
+          <div key={i} className={styles.star} style={{ top: `${Math.random() * 100}%`, left: `${Math.random() * 100}%` }}></div>
+        ))}
+      </div>
     </section>
   );
 };
