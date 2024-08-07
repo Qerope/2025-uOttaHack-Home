6737f63e3fc335564b4b62d28ca5b3b9a3485d82
mbelk059 <mbelk818@gmail.com>
2024-08-07 14:44:22 -0400

    add section ids

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -8,7 +8,7 @@ interface AboutSectionProps {
 
   const AboutSection: React.FC<AboutSectionProps> = () => {
     return (
-        <section className={styles.container}>
+        <section className={styles.container} id="impact">
           <h2 className={styles.title}>Dive into uOttaHack</h2>
         </section>
     );
