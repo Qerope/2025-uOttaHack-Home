d735220fe07c77dd8959bd226346c83aacf34d4d
Qerope <preloopadm@gmail.com>
2024-10-14 19:30:26 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    distDir: 'build', // Change 'build' to any folder name you prefer
+  };
+  
+  export default nextConfig;
+  
