b7d8be198ace808643bddba8094caff99350f487
jwt2706 <jwt2home@gmail.com>
2024-10-22 23:10:59 -0400

    community partners + otrain

--- src/app/components/SponsorsSection.tsx
+++ src/app/components/SponsorsSection.tsx
@@ -8,6 +8,21 @@ import CSE from '../assets/cse.png';
 import surveymonkey from '../assets/surveymonkey.png';
 import deloitte from '../assets/deloitte.png';
 import deepcode from '../assets/deepcode.png';
+import voiceflow from '../assets/voiceflow.png'
+import onepassword from '../assets/1password.png';
+import ehub from '../assets/ehub.png';
+import icons8 from '../assets/icons8.png';
+import eleg from '../assets/eleg.png';
+import xyz from '../assets/xyz.png';
+import interviewcake from '../assets/interviewcake.png';
+import balsamiq from '../assets/balsamiq.png';
+
+import mlh from '../assets/mlh.png';
+import github from '../assets/github.png';
+import uottawa from '../assets/uottawa.png';
+
+import otrain from '../assets/otrain.png';
+import otrainRails from '../assets/otrain_rails.png';
 
 interface SponsorsSectionProps {}
 
@@ -19,65 +34,85 @@ const SponsorsSection: React.FC<SponsorsSectionProps> = () => {
 
             <div className={styles.row}>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={solace} alt="Solace Logo" />
+                    <Image src={solace} alt="Solace Logo" />
                 </div>
             </div>
 
             <div className={styles.row}>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={EEF} alt="EEF Logo" />
+                    <Image src={EEF} alt="EEF Logo" />
                 </div>
             </div>
 
             <div className={styles.row}>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={CSE} alt="CSE Logo" />
+                    <Image src={CSE} alt="CSE Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={surveymonkey} alt="SurveyMonkey Logo" />
+                    <Image src={surveymonkey} alt="SurveyMonkey Logo" />
                 </div>
             </div>
 
             <div className={styles.row}>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deloitte} alt="Deloitte Logo" />
+                    <Image src={deloitte} alt="Deloitte Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={deepcode} alt="DeepCode Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image alt=" Logo" />
                 </div>
             </div>
 
             <div className={styles.row}>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deloitte} alt="Deloitte Logo" />
+                    <Image src={voiceflow} alt="VoiceFlow Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={onepassword} alt="1Password Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={ehub} alt="Entrepreneurship Hub Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={icons8} alt="Icons8 Logo" />
                 </div>
             </div>
 
             <div className={styles.row}>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deloitte} alt="Deloitte Logo" />
+                    <Image src={eleg} alt="Elegoo Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={xyz} alt=".xyz Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={interviewcake} alt="Interview Cake Logo" />
                 </div>
                 <div className={styles.sponsorContainer}>
-                    <Image className={styles.sponsor} src={deepcode} alt="DeepCode Logo" />
+                    <Image src={balsamiq} alt="Balsamiq Logo" />
                 </div>
+            </div>
+
+            <p className={styles.partnersTitle}>Community Partners</p>
+            <div className={styles.row}>
+                <div className={styles.partnersContainer}>
+                    <Image src={mlh} alt="MLH Logo" />
+                </div>
+                <div className={styles.partnersContainer}>
+                    <Image src={github} alt="Github Logo" />
+                </div>
+                <div className={styles.partnersContainer}>
+                    <Image src={uottawa} alt="uOttawa Logo" />
+                </div>
+            </div>
+
+            <div className={styles.otrain} >
+                <Image src={otrain} alt="OTrain Drawing" />
+            </div>
+            <div className={styles.otrainrails} >
+                <Image src={otrainRails} alt="Otrain Rails Drawing" />
             </div>
         </section>
     );
