073ae1a3e2b648787af30817c1a8c12070b6fcb1
mbelk059 <mbelk818@gmail.com>
2024-10-30 17:00:26 -0400

    add uOttaHack Had section + cleanup

--- src/app/components/UOttaHackHad.tsx
+++ src/app/components/UOttaHackHad.tsx
@@ -9,9 +9,22 @@ const UOttaHackHad: React.FC = () => {
     <section className={styles.section}>
       <div className={styles.imageContainer}>
         <img src={RectangleImage.src} alt="UOttaHack Background" className={styles.rectangleImage} />
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles.heading}>uOttaHack 2024 had...</h1>
+        <div className={styles.overlayContent}>
+          <h1 className={styles.heading}>uOttaHack 2024 had...</h1>
+          
+          <div className={styles.statsContainer}>
+            <div className={styles.stat}><h2>2000+</h2><p>Applications</p></div>
+            <div className={styles.stat}><h2>650</h2><p>Hackers</p></div>
+            <div className={styles.stat}><h2>30+</h2><p>Schools</p></div>
+            <div className={styles.stat}><h2>8</h2><p>Doggos</p></div>
+            <div className={styles.stat}><h2>17.1k</h2><p>Prizes</p></div>
+          </div>
+
+          <div className={styles.overlayImages}>
+            <img src={road.src} alt="Road" className={styles.roadImage} />
+            <img src={maps.src} alt="Maps" className={styles.mapImage} />
+          </div>
+        </div>
       </div>
     </section>
   );
