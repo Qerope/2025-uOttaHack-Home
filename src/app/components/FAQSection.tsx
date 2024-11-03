122455bc0b7f06dea3f55997c7db0d134fa43416
Aditya Kandel <addrido@gmail.com>
2024-11-02 21:04:18 -0400

    updated faq

--- src/app/components/FAQSection.tsx
+++ src/app/components/FAQSection.tsx
@@ -1,31 +1,79 @@
 import React, { useState } from "react";
 
 import styles from "../styles/FAQSection.module.css";
-
+import ReactMarkdown from "react-markdown";
+import styled from "@emotion/styled";
 interface FAQSectionProps {
   question: string;
   answer: string;
 }
+const Markdown = styled(ReactMarkdown as any)`
+  * {
+    font-family: Inter;
+    font-size: 14px;
+  }
+
+  a {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
 
 const faqs: FAQSectionProps[] = [
-  { question: "What is a hackathon?", answer: "idk lmao" },
-  { question: "How many people can be on my team?", answer: "idk lmao" },
-  { question: "When will I get my acceptance?", answer: "idk lmao" },
-  { question: "Who can participate?", answer: "idk lmao" },
-  { question: "How much does it cost to attend?", answer: "idk lmao" },
-  { question: "What are mentors?", answer: "idk lmao" },
   {
-    question: "Can I use a pre-existing project I'm working on?",
-    answer: "idk lmao",
+    question: "What is uOttaHack?",
+    answer: `uOttaHack is an MLH hackathon hosted by University of Ottawa students! It's a sprint-like event where students from all across North America will come to turn their ideas into real projects through hardware and software. However, they only have 36 hours to accomplish their goal... Hackers will receive mentorship, learn about new job opportunities, get some cool swag, and have fun along the way!`,
   },
   {
-    question: "What workshops and activities will there be?",
-    answer: "idk lmao",
+    question: "Will uOttaHack happen in person or virtually?",
+    answer: `No. After 2 years the hackathon will be hosted on the beautiful campus of the University of Ottawa and will take place from February 10th - 12th, 2023. Don’t worry about getting lost! We'll have plenty of signage and volunteers around campus to guide you. We can't wait to see you there!`,
   },
-  { question: "Can I reuse my past hackathon projects?", answer: "idk lmao" },
-  { question: "Where do I submit my projects?", answer: "idk lmao" },
-  { question: "Is there an age restriction?", answer: "idk lmao" },
-  { question: "How do I apply?", answer: "idk lmao" },
+  {
+    question: "Who can participate?",
+    answer: `Any students, at any skill level, from any field of study are welcome! The only thing you need is an interest in building something amazing and the motivation to learn!
+
+\\
+**Are you a recent grad?** No worries - as long as you've graduated in the last 12 months, you're welcome to apply and attend!`,
+  },
+  {
+    question: "How much will it cost me?",
+    answer: `uOttaHack has no admission fees and all meals and benefits during the hackathon are free. Did we also mention that each hacker will be getting an amazing swag bag?`,
+  },
+  {
+    question: "What if I've never been to a hackathon before?",
+    answer: `There's a first time for everything, right? Hackathons are all about learning new things, meeting people, and having an amazing experience. 
+    We'll have plenty of workshops and mentors during the event to help you out! At our last iteration, 30% of our participants were first-time hackers!`,
+  },
+  {
+    question: "Can I start working on a project before the event?",
+    answer: `All projects must be started from scratch at the event, but you're free to start brainstorming ideas beforehand!`,
+  },
+  {
+    question: "Can I participate as part of a team?",
+    answer: `**Yes**! Hacking can be done solo, or in a team of up to four participants.`,
+  },
+  {
+    question: "Can high school students come?",
+    answer: `**Absolutely!** We highly encourage high school students to apply! Minors attending will be asked to have waivers signed by their parents/guardians.`,
+  },
+  {
+    question: "Sounds great, but what do I win?",
+    answer: `We started uOttaHack to empower students in creating innovative, fun, and awesome projects, without the stress of a regular semester's weekend. The biggest rewards you will get are the friends you make that will last a lifetime and an experience you won't forget.
+**But, with that said** - you'll have the chance to demo to our stellar panel of judges and the top teams will be awarded sweet prizes! There will also be smaller prizes for specific categories and some given out by our sponsors! In the past, hackers have won DJI drones, Nintendo Switches, and Fitbit smart watches!`,
+  },
+  {
+    question: "What is Major League Hacking?",
+    answer: `[Major League Hacking](https://mlh.io/) is the official student hackathon league in North America & Europe. They work with over 200 member events and empower over 70,000 students every year.`,
+  },
+  {
+    question: "Is there a code of conduct?",
+    answer: `Yes there is. The organizers care deeply about everyone's experience and ensuring they get to hack in a comfortable and safe environment. You can find it [here](https://static.mlh.io/docs/mlh-code-of-conduct.pdf).`,
+  },
+  {
+    question: "I don't live in Ottawa, can I still go?",
+    answer: `uOttaHack will be providing partial reimbursement to most major cities in and surrounding Ontario! Accepted attendees will need to confirm ahead of time should reimbursement services be required, and will need to submit a project. Otherwise travelling to uOttaHack will be at the expense of the participant. This info will be sent to you if you are accepted!`,
+  },
 ];
 
 const FAQSection: React.FC = () => {
@@ -44,7 +92,7 @@ const FAQSection: React.FC = () => {
         </h3>
       </div>
       <div
-        className={`${styles.faqContainer} backdrop-blur-lg rounded p-10 flex justify-center text-sm items-center h-full`}
+        className={`${styles.faqContainer} backdrop-blur-lg  rounded p-10 flex justify-center text-sm items-center h-full`}
       >
         {faqs.map((faq, index) => (
           <div key={index} className={`${styles.faqItem} pt-10`}>
@@ -60,7 +108,9 @@ const FAQSection: React.FC = () => {
               ></div>
             </div>
             {openIndex === index && (
-              <p className={styles.faqAnswer}>{faq.answer}</p>
+              <p className={` ${styles.faqAnswer} pt-10 leading-wider`}>
+                <Markdown>{faq.answer}</Markdown>
+              </p>
             )}
           </div>
         ))}
