2f978d7ed5ff9f2b16273cc56d6f2d2661ec6f04
Aditya Kandel <addrido@gmail.com>
2024-11-07 18:07:33 -0500

    updated mlh badge

--- src/app/components/MainHeader.tsx
+++ src/app/components/MainHeader.tsx
@@ -128,12 +128,28 @@ const MainHeader = () => {
         </div>
 
         {/* MLH Badge */}
-        <div className="absolute right-[10%] top-1/2 transform -translate-y-1/2  md:block">
-          <img
-            src={mlh.src}
-            alt="MLH 2025 Badge"
-            className="h-20 hidden md:block md:h-48 md:mt-20  w-auto"
-          />
+        <div className="absolute ">
+          <a
+            id="mlh-trust-badge"
+            style={{
+              display: "block",
+              maxWidth: "100px",
+              minWidth: "60px",
+              position: "fixed",
+              right: "50px",
+              top: "0",
+              width: "10%",
+              zIndex: 10000,
+            }}
+            href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white"
+            target="_blank"
+          >
+            <img
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg"
+              alt="Major League Hacking 2025 Hackathon Season"
+              style={{ width: "100%" }}
+            />
+          </a>
         </div>
       </div>
 
